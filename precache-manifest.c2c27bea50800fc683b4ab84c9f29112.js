9b8ccbef1b58136aff57dd65cc5c83b5aed9087e
Nicolas Martiné <nicolas57415@gmail.com>
2020-01-10 15:04:36 +0100

    Updates

---
+++ precache-manifest.c2c27bea50800fc683b4ab84c9f29112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4414cdcf0bce07f20aec8f33a2255e",
+    "url": "/lyrics-search/index.html"
+  },
+  {
+    "revision": "196e813f53018df160f9",
+    "url": "/lyrics-search/static/css/main.bd3d68fa.chunk.css"
+  },
+  {
+    "revision": "f12a02b8fdaea0810428",
+    "url": "/lyrics-search/static/js/2.d8e35b15.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/lyrics-search/static/js/2.d8e35b15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "196e813f53018df160f9",
+    "url": "/lyrics-search/static/js/main.813ce6e0.chunk.js"
+  },
+  {
+    "revision": "080ba871a45e1921a0b2",
+    "url": "/lyrics-search/static/js/runtime-main.3e50bb64.js"
+  },
+  {
+    "revision": "24d95baa29431b226e07e9279d19b972",
+    "url": "/lyrics-search/static/media/31.24d95baa.gif"
+  }
+]);
